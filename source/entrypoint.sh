5a0d71011c6531f3ab652b560fd02bfb6596f162
BadAimWeeb <badaimweeb@protonmail.com>
2026-06-26 20:24:24 -0500

    correctly restore third party docs

--- source/entrypoint.sh
+++ source/entrypoint.sh
@@ -12,11 +12,14 @@ ensure_freepbx_cli_links() {
   done
 }
 
-# If FreePBX is installed (by detecting fwconsole), trigger restart of FreePBX service to ensure all services are running
+# If FreePBX is installed (by detecting fwconsole), trigger start of FreePBX service to ensure all services are running
 make_sure_freepbx_is_running() {
   if [[ -x /usr/sbin/fwconsole ]]; then
-    echo "FreePBX detected, ensuring FreePBX services are running..."
+    echo "FreePBX detected, ensuring FreePBX services are running (including Asterisk)..."
     /usr/sbin/fwconsole start || true
+  else
+    echo "FreePBX not detected, running Asterisk only."
+    /usr/local/src/freepbx/start_asterisk start
   fi
 }
 
@@ -25,7 +28,7 @@ ensure_freepbx_cli_links
 # Restoring backup of Opus and third-party documentation
 if [[ -d /image_backup/asterisk_documentation_thirdparty ]]; then
   echo "Restoring backup of Opus and third-party documentation..."
-  cp -r /image_backup/asterisk_documentation_thirdparty /var/lib/asterisk/documentation/thirdparty
+  cp -r /image_backup/asterisk_documentation_thirdparty/*.* /var/lib/asterisk/documentation/thirdparty/
 fi
 
 # Start cron
@@ -34,7 +37,7 @@ fi
 # Start postfix email service
 service postfix start
 
-# Start Asterisk service
-(/usr/local/src/freepbx/start_asterisk start && make_sure_freepbx_is_running) &
+# Start Asterisk & FreePBX service
+make_sure_freepbx_is_running &
 
 exec apache2ctl -D FOREGROUND
